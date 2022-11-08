00f413a8b0c1ea72e6c5ea44e9f5d2c046addac9
Cihan <cihannakgul@gmail.com>
2022-11-08 17:07:03 +0300

    Added feed, settings and send post viewcontrollers

--- instaCloneFirebase/AppDelegate.swift
+++ instaCloneFirebase/AppDelegate.swift
@@ -6,7 +6,10 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- instaCloneFirebase/ViewController.swift
+++ instaCloneFirebase/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("first git test")
     }
-
+    
+    
+    @IBAction func SignUpClicked(_ sender: Any) {
+    }
+    
+    
+    
+    
+    @IBAction func SignInClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
 
 }
 
