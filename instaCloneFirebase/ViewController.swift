0d8bb0acab5fcdec84b60a5f262e2a0dcffa4153
Cihan <cihannakgul@gmail.com>
2022-11-08 14:03:49 +0300

    Initial Commit

---
+++ instaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaCloneFirebase
+//
+//  Created by cihan on 08.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
