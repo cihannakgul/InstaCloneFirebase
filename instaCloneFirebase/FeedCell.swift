57ae7494fc844a5dafcdac82d87a95fbf0d760d3
Cihan <cihannakgul@gmail.com>
2022-11-14 19:26:10 +0300

    last changes, fixing bug to like your own post

--- instaCloneFirebase/FeedCell.swift
+++ instaCloneFirebase/FeedCell.swift
@@ -21,6 +21,8 @@ class FeedCell: UITableViewCell {
     
     @IBOutlet weak var lblDocumentId: UILabel!
     
+    var PostAuthor :String!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -31,14 +33,54 @@ class FeedCell: UITableViewCell {
 
      
     }
+    
+    func CreateMessage(Message:String){
+        let alert = UIAlertController(title: "Hey!!", message: Message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.window?.rootViewController?.present(alert, animated: true)
+    }
 
+    
+   func isItMeToLiked(author : String){
+       if let likeCount = Int(lblLikes.text!){
+           let firestoreDatabase = Firestore.firestore()
+            
+           
+           if author == Auth.auth().currentUser?.email as? String{
+               self.CreateMessage(Message:  "You cannot like your own post!")
+           }else{
+               
+               let likeStore = ["likes" : likeCount+1] as [String : Any]
+               
+               firestoreDatabase.collection("Posts").document(lblDocumentId.text!).setData(likeStore, merge: true)
+               
+               self.CreateMessage(Message: "You liked \(author)'s post")
+           }
+           
+            
+           
+       }
+        
+    }
     @IBAction func likeClicked(_ sender: Any) {
        
         let firestoreDatabase = Firestore.firestore()
-        if let likeCount = Int(lblLikes.text!){
-            let likeStore = ["likes" : likeCount+1] as [String : Any]
+        let username = firestoreDatabase.collection("Posts").document(lblDocumentId.text!).getDocument { document, error in
             
-            firestoreDatabase.collection("Posts").document(lblDocumentId.text!).setData(likeStore, merge: true)
+            guard let document = document, document.exists else {
+                            print("Document does not exist")
+                            return
+                        }
+                        let dataDescription = document.data()
+            if let postby = dataDescription?["postedBy"] as? String{
+                self.PostAuthor = postby
+                
+                self.isItMeToLiked(author: self.PostAuthor)
+                
+            }
+                        
         }
+     
     }
 }
